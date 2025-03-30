ec5c51b556a4866ebe4476d40fa6ad304eb3febc
unknown <kf53844@zut.edu.pl>
2025-03-30 21:41:30 +0200

    Skonczenie

--- bezpieczny-dach/src/index.js
+++ bezpieczny-dach/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.title = "BezpiecznyDach"; 
+
+
 root.render(
   <React.StrictMode>
     <App />
